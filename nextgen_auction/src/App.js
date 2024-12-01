8fdae1d26f8ccca3eaf1b78d32a4aa66c5ff17f2
Shashi-Tej-Reddy-Singa-Reddy <shashitejreddysingareddy@gmail.com>
2024-12-01 12:03:40 +0530

    kk

--- nextgen_auction/src/App.js
+++ nextgen_auction/src/App.js
@@ -3,12 +3,19 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navabar';
+import Login from './components/Login';
+import Home from './components/Home';
 
 function App() {
     return (
         <Router>
             <div>
                 <Navbar />
+                <main>
+                    <Routes>
+                        <Route path="/" element={<Home/>}/>
+                    </Routes>
+                </main>
             </div>
         </Router>
     );
